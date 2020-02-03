7639313e30bb5eb628844301f9f704c31f7aee8f
Abhijeet <abhijeetdtu@gmail.com>
2020-02-03 18:46:02 -0500

    Adding Hydrogen EDA

--- Datathon/Utils/getData.py
+++ Datathon/Utils/getData.py
@@ -15,9 +15,31 @@ DICTDF
 
 DICTDF["Category"].unique()
 
+DICTDF["Data Type"].unique()
+DICTDF.groupby("Category").head(5)
+
+def _columnDtypeCorrection(c):
+    dtype = list(DICTDF[DICTDF["Variable Name"] == c]["Data Type"])[0]
+    if c.find("_id") > -1:
+        dtype = "binary"
+    if c.find("bmi") > -1:
+        dtype = "float"
+
+    return dtype
 
 def getTrainingData():
-    return pd.read_csv(trainDataPath)
+    df =  pd.read_csv(trainDataPath)
+    for c in df.columns:
+        dtype = _columnDtypeCorrection(c)
+        if dtype == "string" or dtype == "binary":
+            df[c] = df[c].astype("category")
+        elif dtype == "integer" and len(df[c].unique()) < 5:
+            df[c] = df[c].astype("category")
+        else:
+            df[c] = df[c].astype("float")
+    return df
+
+
 
 def getColumnTypes():
     return DICTDF["Category"].unique()
@@ -26,7 +48,13 @@ def getColumnTypes():
 def getColumnsByType(type):
     return list(DICTDF[DICTDF["Category"] == type]["Variable Name"])
 
+def getDependentVariable():
+    return "hospital_death"
+
 getColumnTypes()
 
 
 getColumnsByType("demographic")
+
+getTrainingData().columns
+DICTDF[DICTDF["Variable Name"] == "bmi"]
